41cd5ec6e095232475dd07882d1da761750bacde
karina-talibzhanova <karina.talibzhanova@gmail.com>
2021-10-22 12:18:31 +0100

    added employee DTO to avoid exposing personal information (e.g. address, home phone, birth date)

--- src/main/java/com/sparta/kt/session3/controllers/EmployeeController.java
+++ src/main/java/com/sparta/kt/session3/controllers/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.sparta.kt.session3.controllers;
 
+import com.sparta.kt.session3.dtos.CustomerDTO;
+import com.sparta.kt.session3.dtos.EmployeeDTO;
 import com.sparta.kt.session3.entities.EmployeeEntity;
 import com.sparta.kt.session3.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
@@ -24,16 +27,16 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     @ResponseBody
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) String name) {
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false) String name) {
         if (name == null) {
-            return employeeRepository.findAll();
+            return employeeRepository.findAll().stream().map(EmployeeDTO::new).collect(Collectors.toList());
         }
 
-        List<EmployeeEntity> foundEmployees = new ArrayList<>();
+        List<EmployeeDTO> foundEmployees = new ArrayList<>();
 
         for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
             if ((employeeEntity.getFirstName() + employeeEntity.getLastName()).contains(name)) {
-                foundEmployees.add(employeeEntity);
+                foundEmployees.add(new EmployeeDTO(employeeEntity));
             }
         }
 
@@ -53,12 +56,12 @@ public class EmployeeController {
 
     @GetMapping(value="/employees/title", params = {"contactTitle"})
     @ResponseBody
-    public List<EmployeeEntity> getAllEmployeesByContactTitle(@RequestParam String contactTitle) {
-        List<EmployeeEntity> foundEmployees = new ArrayList<>();
+    public List<EmployeeDTO> getAllEmployeesByContactTitle(@RequestParam String contactTitle) {
+        List<EmployeeDTO> foundEmployees = new ArrayList<>();
 
         for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
             if (employeeEntity.getTitle().equalsIgnoreCase(contactTitle)) {
-                foundEmployees.add(employeeEntity);
+                foundEmployees.add(new EmployeeDTO(employeeEntity));
             }
         }
 
@@ -67,28 +70,28 @@ public class EmployeeController {
 
     @GetMapping("/employees/location")
     @ResponseBody
-    public List<EmployeeEntity> getAllEmployeesInSpecificLocation(@RequestParam(required = false) String region, @RequestParam(required = false) String country, @RequestParam(required = false) String city) {
+    public List<EmployeeDTO> getAllEmployeesInSpecificLocation(@RequestParam(required = false) String region, @RequestParam(required = false) String country, @RequestParam(required = false) String city) {
         if (region == null && country == null && city == null) {
             // something has gone wrong here, need to report an error
             return new ArrayList<>();
         }
-        List<EmployeeEntity> foundEmployees = new ArrayList<>();
+        List<EmployeeDTO> foundEmployees = new ArrayList<>();
 
         if (region != null) {
             for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
                 if (employeeEntity.getRegion()!=null && employeeEntity.getRegion().equalsIgnoreCase(region)) {
-                    foundEmployees.add(employeeEntity);
+                    foundEmployees.add(new EmployeeDTO(employeeEntity));
                 }
             }
         }
 
         if (country != null) {
             if (foundEmployees.size() > 0) {
-                foundEmployees.removeIf(employeeEntity -> !employeeEntity.getCountry().equalsIgnoreCase(country));
+                foundEmployees.removeIf(employeeDTO -> !employeeDTO.getCountry().equalsIgnoreCase(country));
             } else {
                 for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
                     if (employeeEntity.getCountry() != null && employeeEntity.getCountry().equalsIgnoreCase(country)) {
-                        foundEmployees.add(employeeEntity);
+                        foundEmployees.add(new EmployeeDTO(employeeEntity));
                     }
 
                 }
@@ -97,11 +100,11 @@ public class EmployeeController {
 
         if (city != null) {
             if (foundEmployees.size() > 0) {
-                foundEmployees.removeIf(employeeEntity -> !employeeEntity.getCity().equalsIgnoreCase(city));
+                foundEmployees.removeIf(employeeDTO -> !employeeDTO.getCity().equalsIgnoreCase(city));
             } else {
                 for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
                     if (employeeEntity.getCity() != null && employeeEntity.getCity().equalsIgnoreCase(city)) {
-                        foundEmployees.add(employeeEntity);
+                        foundEmployees.add(new EmployeeDTO(employeeEntity));
                     }
                 }
             }
@@ -111,13 +114,13 @@ public class EmployeeController {
 
     @GetMapping("/employees/hired")
     @ResponseBody
-    public List<EmployeeEntity> getAllEmployeesHiredBetweenDates(@RequestParam(required = false) String lowerBound, @RequestParam(required = false) String upperBound) {
+    public List<EmployeeDTO> getAllEmployeesHiredBetweenDates(@RequestParam(required = false) String lowerBound, @RequestParam(required = false) String upperBound) {
         if (lowerBound == null && upperBound == null) {
             // something has gone wrong
             return new ArrayList<>();
         }
 
-        List<EmployeeEntity> foundEmployees = new ArrayList<>();
+        List<EmployeeDTO> foundEmployees = new ArrayList<>();
         LocalDate lowerDate = LocalDate.MIN;
         LocalDate upperDate = LocalDate.MAX;
 
@@ -130,7 +133,7 @@ public class EmployeeController {
 
         for (EmployeeEntity employeeEntity : employeeRepository.findAll()) {
             if (employeeEntity.getHireDate().isAfter(lowerDate) && employeeEntity.getHireDate().isBefore(upperDate)) {
-                foundEmployees.add(employeeEntity);
+                foundEmployees.add(new EmployeeDTO(employeeEntity));
             }
         }
 
---
+++ src/main/java/com/sparta/kt/session3/dtos/EmployeeDTO.java
@@ -0,0 +1,77 @@
+package com.sparta.kt.session3.dtos;
+
+import com.sparta.kt.session3.entities.EmployeeEntity;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO {
+    private String lastName;
+    private String firstName;
+    private String title;
+    private String titleOfCourtesy;
+    private LocalDate hireDate;
+    private String city;
+    private String region;
+    private String country;
+    private String notes;
+    private EmployeeDTO reportsTo;
+    private Double salary;
+
+    public EmployeeDTO(EmployeeEntity employeeEntity) {
+        lastName = employeeEntity.getLastName();
+        firstName = employeeEntity.getFirstName();
+        title = employeeEntity.getTitle();
+        titleOfCourtesy = employeeEntity.getTitleOfCourtesy();
+        hireDate = employeeEntity.getHireDate();
+        city = employeeEntity.getCity();
+        region = employeeEntity.getRegion();
+        country = employeeEntity.getCountry();
+        notes = employeeEntity.getNotes();
+        reportsTo = employeeEntity.getReportsTo() == null ? null : new EmployeeDTO(employeeEntity.getReportsTo());
+        salary = employeeEntity.getSalary();
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTitleOfCourtesy() {
+        return titleOfCourtesy;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public EmployeeDTO getReportsTo() {
+        return reportsTo;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+}
